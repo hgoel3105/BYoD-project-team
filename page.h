f02fcc86627a75efc388608f45f1082e2c19813d
Shreevardhan-hack04 <agarwalshreevardhan04@gmail.com>
2025-06-30 12:16:30 +0530

    Update page.h

--- page.h
+++ page.h
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include"row.h"
-#define size 4096
-#define ele 60
+#define size 4096 //size of one page
+#define ele 60 //size of row data
 //array of pointers to each row inserted in a single page
 typedef struct page{
     Row* row_ptr[size/ele];
