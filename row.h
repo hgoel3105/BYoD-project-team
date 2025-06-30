bdad05d86e5f9303b61313121d3ba16f0c620597
Shreevardhan-hack04 <agarwalshreevardhan04@gmail.com>
2025-06-30 12:09:13 +0530

    Add files via upload

---
+++ row.h
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#define MAX 20
+#define code 4
+#define loc 12
+struct row{
+    int ID;
+    char name[MAX];
+    char branch[code];
+    char city[loc];
+    int MTH;
+    int PHY;
+    int CHM;
+    int TA;
+    int LIF;
+}Row;
