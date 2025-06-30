8f080f5f044b90ce45a4b1c469396f5bda41dd03
Shreevardhan-hack04 <agarwalshreevardhan04@gmail.com>
2025-06-30 12:11:18 +0530

    Update row.h

--- row.h
+++ row.h
@@ -2,11 +2,13 @@
 #define MAX 20
 #define code 4
 #define loc 12
+//data of each row 
 struct row{
-    int ID;
-    char name[MAX];
-    char branch[code];
-    char city[loc];
+    int ID;//student's ID
+    char name[MAX];//student's name
+    char branch[code];//student's branch in acronym
+    char city[loc];//studnet's city
+    //marks of the student in different courses
     int MTH;
     int PHY;
     int CHM;
